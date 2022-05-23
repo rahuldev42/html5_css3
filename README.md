92c326678d16f621075ff813bd9ec39a4d6835d0
rahuldev42 <rahulmudam.iiitg@gmail.com>
2022-05-23 20:34:02 +0530

    adding the html files

---
+++ README.md
@@ -0,0 +1,2 @@
+# html5-css3-23052022
+A repo to explore HTML5 and CSS3
---
+++ app.js
@@ -0,0 +1,8 @@
+  //alert('Hello Guys, here is your javascript')
+  function sayHello(){
+    return "Welcome to Javascript"
+}
+
+console.log(sayHello())
+
+document.getElementById("header").innerHTML ="<h2>Hello</h2>"
